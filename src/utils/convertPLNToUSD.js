53b808939d7f4de4def2810370fe70deb96fafd8
Wojciech Wyskiel <wojtek12.1.96@wp.pl>
2024-01-11 14:48:17 +0100

    NaN test

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,7 +1,10 @@
 export const convertPLNToUSD = (PLN) => {
+  if(typeof PLN === 'string') {
+    return NaN;
+  }
 
   const PLNtoUSD = PLN / 3.5;
-  
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
